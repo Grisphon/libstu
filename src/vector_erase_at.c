093a520bb0c2ce9499b480193db5b1520b427ad2
Quentin <quentin.mettier@edu.ecole-89.com>
2024-04-19 14:29:06 +0200

    vector erase at fixed

--- src/vector_erase_at.c
+++ src/vector_erase_at.c
@@ -7,10 +7,16 @@ void vector_erase_at(struct vector *ve, unsigned int pos)
     char *new_arr;
 
     new_arr = (char *) ve->array;
-    if (ve->dtor)
-        ve->dtor(&new_arr[ve->size_elem * pos]);
-    stu_memmove(&new_arr[pos * ve->size_elem],
-                &new_arr[(pos + 1) * ve->size_elem],
-                (ve->actual_size - pos) * ve->size_elem);
-    ve->actual_size -= 1;
+    if (pos == 0) {
+        vector_pop_front(ve);
+    } else if ((int) pos > 0 && pos < ve->actual_size - 1) {
+        if (ve->dtor)
+            ve->dtor(&new_arr[ve->size_elem * pos]);
+        stu_memmove(&new_arr[pos * ve->size_elem],
+                    &new_arr[(pos + 1) * ve->size_elem],
+                    (ve->actual_size - pos) * ve->size_elem);
+        ve->actual_size -= 1;
+    } else {
+        vector_pop_back(ve);
+    }
 }
