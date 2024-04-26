4f6911e7ed62e3152dce38cbe5ee9e64a100a128
Ilan Trigueiro Legrand <ilan.trigueiro-legrand@edu.ecole-89.com>
2024-04-26 16:38:20 +0200

    corrected

--- src/memmove.c
+++ src/memmove.c
@@ -1,24 +1,46 @@
+/*
+ * E89 Pedagogical & Technical Lab
+ * project:     memory
+ * created on:  2024-02-07 - 11:15 +0100
+ * 1st author:  ilan.trigueiro-legrand - ilan.trigueiro-legrand
+ * description: memmove
+ */
+
+static void *left(void *dest, const void *src, unsigned int n)
+{
+    char *dest1;
+    char *src1;
+    unsigned int count;
+
+    dest1 = (char *) dest;
+    src1 = (char *) src;
+    count = 0;
+    while (count < n) {
+        dest1[count] = src1[count];
+        count += 1;
+    }
+    return dest1;
+}
+
+static void *right(void *dest, const void *src, unsigned int n)
+{
+    char *dest1;
+    char *src1;
+
+    dest1 = (char *) dest;
+    src1 = (char *) src;
+    while (n > 0) {
+        dest1[n - 1] = src1[n - 1];
+        n -= 1;
+    }
+    return dest1;
+}
+
 void *stu_memmove(void *dest, const void *src, unsigned int n)
 {
-    unsigned int count;
-    char *new_dest;
-    char *new_src;
-
-    new_dest = (char *) dest;
-    new_src = (char *) src;
-    count = 0;
     if (dest > src) {
-        while (count <= n) {
-            if (n - count < n)
-                new_dest[n - count] = new_src[n - count];
-            count += 1;
-        }
-    } else if (dest < src){
-        while (count < n) {
-            if (count - 1 > 0)
-                new_dest[count] = new_src[count];
-            count += 1;
-        }
+        return right(dest, src, n);
+    } else {
+        return left(dest, src, n);
     }
-    return dest;
 }
